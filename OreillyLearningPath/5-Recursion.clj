dcff7846bbf9d27174e8a48798cf1cd6e16afbf0
xrecoba <xavi.ametller@gmail.com>
2016-10-13 17:39:21 +0200

    Recursion examples

--- OreillyLearningPath/5-Recursion.clj
+++ OreillyLearningPath/5-Recursion.clj
@@ -45,4 +45,10 @@
 (reduce + [3])
 
 
+;;Filters can be done with reduce, but filter function works better
+(filter even? [0 1 2 3 4 5 6 7 8])
 
+;; same goes for map and group
+(map inc [0 1 2 3 4 5 6 7 8])
+
+(group-by #(if (even? %) :even :odd)  [0 1 2 3 4 5 6 7 8])
